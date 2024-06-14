6018fde0b6d89c3954f166081118c6cfeb794b96
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-06-14 12:45:16 +0700

    feat [user.controller]: added function to get userList

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,35 @@
+const { User } = require("../models");
+
+const userList = async (req, res, _next) => {
+  try {
+    const users = await User.findAll({
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+
+    if (users.length === 0) {
+      return res.status(404).send({
+        success: false,
+        message: "No users found",
+        data: null,
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Users retrieved successfully",
+      data: users,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
+module.exports = {
+  userList,
+}
