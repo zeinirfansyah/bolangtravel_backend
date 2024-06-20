2def747e099a39594db156474f509beb3c336237
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-06-20 08:10:16 +0700

    feat [get-user-by-id]: added get user by Id in a controller and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,6 +30,39 @@ const userList = async (req, res, _next) => {
   }
 };
 
+const userById = async (req, res, _next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { id },
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User not found",
+        data: null,
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "User retrieved successfully",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
 module.exports = {
   userList,
-}
+  userById,
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { verifyToken, authorizeRole } = require("../middleware/auth");
-const { userList } = require("../controllers/user.controller");
+const { userList, userById } = require("../controllers/user.controller");
 
 const router = express.Router();
 
 router.get("/users", verifyToken, authorizeRole("admin"), userList);
+router.get("/users/:id", verifyToken, authorizeRole("admin"), userById);
 
 module.exports = router;
