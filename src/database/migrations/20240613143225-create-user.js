2ffb2e18a9e46ac7c176d65b59119dec97850866
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-06-21 14:25:11 +0700

    refactor [migrations/user]: fixed format name for fullname

--- src/database/migrations/20240613143225-create-user.js
+++ src/database/migrations/20240613143225-create-user.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      fullName: {
+      fullname: {
         type: Sequelize.STRING
       },
       username: {
