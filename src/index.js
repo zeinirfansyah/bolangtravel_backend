a97c28ccf70963b65f152b31c50e89d6d5f646e5
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-06-26 21:43:26 +0700

    refactor [/index.js]: added express.static for public folder

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors);
+app.use(express.static('public'));
 
 app.use("/api/auth", authRouter)
 app.use("/api/", routes)
