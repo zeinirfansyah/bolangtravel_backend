365bf18fc8a052e7564ef4dd647462dc0c35c61f
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-06-30 21:57:45 +0700

    feat [travel-package]: added delete travel-package

--- src/routes/travel_package.routes.js
+++ src/routes/travel_package.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { verifyToken, authorizeRole } = require("../middleware/auth");
-const { getAllTravelPackages, getTravelPackageById, createTravelPackage } = require("../controllers/travel-package.controller");
+const { getAllTravelPackages, getTravelPackageById, createTravelPackage, deleteTravelPackage } = require("../controllers/travel-package.controller");
 
 const router = express.Router();
 
 router.get("/table/:limit&:pages", getAllTravelPackages);
 router.get("/:id", getTravelPackageById);
 router.post("/", verifyToken, authorizeRole("admin"), createTravelPackage);
+router.delete("/:id", verifyToken, authorizeRole("admin"), deleteTravelPackage);
+
 
 
 module.exports = router;
