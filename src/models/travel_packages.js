bb282fee6f57eb69371036b4bec9f984001e28a2
Farahksyu <ksyu.farah@gmail.com>
2024-07-03 20:50:31 +0700

    refactor [travel_packages.js] : fixed typos

--- src/models/travel_packages.js
+++ src/models/travel_packages.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
 
-      Travel_Packages.hasMany(models.Booking, {
+      Travel_Packages.hasMany(models.Bookings, {
         as: 'bookings',
         foreignKey: 'travel_package_id',
       });
