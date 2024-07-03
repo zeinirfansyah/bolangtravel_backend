d6b7777597a5865ee416682799b9a4e337c7f9bb
ilhamraffi <ilhamraffi882@gmail.com>
2024-07-03 20:21:45 +0700

    feat [booking.controller.js]: menambahkan completeBooking function

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -1,4 +1,6 @@
 const { Bookings, Travel_Packages, Users } = require("../models");
+const configureMulter = require("../utils/helpers/multer-config");
+const uploadThumbnail = configureMulter("transfer_receipt").single("transfer_receipt");
 
 const createBooking = async (req, res, _next) => {
   const { date } = req.body;
@@ -59,4 +61,96 @@ const createBooking = async (req, res, _next) => {
   }
 };
 
-module.exports = { createBooking };
+const completeBooking = async (req, res, _next) => {
+  try {
+    uploadThumbnail(req, res, async (err) => {
+      if (err) {
+        return res.status(500).send({
+          success: false,
+          message: err.message,
+          data: null,
+        });
+      }
+
+      const { id } = req.params;
+
+      const booking = await Bookings.findOne({ where: { id } });
+      
+      if (!booking) {
+        return res.status(404).send({
+          success: false,
+          message: "Booking not found",
+        });
+      }
+
+      const { bank_name, payer_name } = req.body;
+      const transfer_receipt = req.file
+        ? `/uploads/transfer_receipt/${req.file.filename}`
+        : booking.transfer_receipt;
+      const { id: user_id } = req.user;
+
+      if (!bank_name || !payer_name || !transfer_receipt) {
+        return res.status(400).send({
+          success: false,
+          message: "Bank name, payer name, and transfer receipt are required",
+        });
+      }
+
+      if (booking.user_id !== user_id) {
+        return res.status(403).send({
+          success: false,
+          message: "You are not authorized to complete this booking",
+        });
+      }
+
+      if (booking.status == "pending") {
+        return res.status(403).send({
+          success: false,
+          message: "Booking is already pending, please wait for confirmation",
+        });
+      }
+
+      if (booking.status == "paid") {
+        return res.status(403).send({
+          success: false,
+          message: "Booking is already completed",
+        });
+      }
+
+      if (booking.status == "failed") {
+        return res.status(403).send({
+          success: false,
+          message:
+            "You cant complete a failed booking, please contact admin or make a new booking",
+        });
+      }
+
+      if (booking.transfer_receipt) {
+        return res.status(400).send({
+          success: false,
+          message: "Transfer receipt already uploaded",
+        });
+      }
+
+      const completedBooking = await booking.update({
+        bank_name,
+        payer_name,
+        transfer_receipt,
+        status: "pending",
+      });
+
+      return res.status(200).send({
+        success: true,
+        message: "Payment is success, please wait for confirmation",
+        data: completedBooking,
+      });
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: `Internal server error: ${error.message}`,
+    });
+  }
+};
+
+module.exports = { createBooking, completeBooking };
