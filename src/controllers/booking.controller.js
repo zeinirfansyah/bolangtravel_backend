684eea466b7691c6853584bc44ed3e8af20c803e
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-05 21:42:24 +0700

    feat [booking]: get all booking, complete booking, get all booking history for authenticated user, and get booking history by id

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -1,52 +1,47 @@
+const moment = require("moment");
+
 const { Bookings, Travel_Packages, Users } = require("../models");
 const configureMulter = require("../utils/helpers/multer-config");
-const uploadThumbnail = configureMulter("transfer_receipt").single("transfer_receipt");
+const uploadThumbnail =
+  configureMulter("transfer_receipt").single("transfer_receipt");
 
 const createBooking = async (req, res, _next) => {
   const { date } = req.body;
   const { travel_package_id } = req.query;
   const { id } = req.user;
 
-  try {
-    if (!date) {
-      return res.status(400).send({
-        success: false,
-        message: "Date is required",
-      });
-    }
+  const package = await Travel_Packages.findOne({
+    where: { id: travel_package_id },
+  });
+  const user = await Users.findOne({ where: { id } });
 
-    const package = await Travel_Packages.findOne({
-      where: { id: travel_package_id },
+  if (!date) {
+    return res.status(400).send({
+      success: false,
+      message: "Date is required",
     });
+  }
 
-    const user = await Users.findOne({ where: { id } });
+  if (!package) {
+    return res.status(404).send({
+      success: false,
+      message: "Travel package not found",
+    });
+  }
 
-    if (!package) {
-      return res.status(404).send({
-        success: false,
-        message: "Travel package not found",
-      });
-    }
+  if (!user) {
+    return res.status(404).send({
+      success: false,
+      message: "User not found",
+    });
+  }
 
-    if (!user) {
-      return res.status(404).send({
-        success: false,
-        message: "User not found",
-      });
-    }
-
+  try {
     const newBooking = await Bookings.create({
       date,
       user_id: id,
       travel_package_id,
     });
-
-    if (!newBooking) {
-      return res.status(500).send({
-        success: false,
-        message: "Error creating booking",
-      });
-    }
 
     return res.status(201).send({
       success: true,
@@ -57,6 +52,131 @@ const createBooking = async (req, res, _next) => {
     return res.status(500).send({
       success: false,
       message: `Internal server error: ${error.message}`,
+    });
+  }
+};
+
+const getAllBookings = async (req, res, _next) => {
+  try {
+    const { limit, pages } = req.params;
+
+    const limit_int = parseInt(limit);
+    const pages_int = parseInt(pages);
+
+    const offset = (pages_int - 1) * limit_int;
+
+    const bookings = await Bookings.findAndCountAll({
+      limit: limit_int,
+      offset: offset,
+      include: [
+        {
+          model: Travel_Packages,
+          as: "travel_packages",
+          attributes: {
+            exclude: ["created_at", "updated_at"],
+          },
+        },
+      ],
+      attributes: {
+        exclude: ["created_at", "updated_at"],
+      },
+    });
+
+    if (!bookings) {
+      return res.status(404).send({
+        success: false,
+        message: "No bookings found",
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Bookings retrieved successfully",
+      data: bookings,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: `Error: ${error.message}`,
+    });
+  }
+};
+
+const getBookingHistories = async (req, res, _next) => {
+  try {
+    const { id } = req.user;
+    const bookings = await Bookings.findAll({
+      where: { user_id: id },
+      include: [
+        {
+          model: Travel_Packages,
+          as: "travel_packages",
+          attributes: {
+            exclude: ["created_at", "updated_at"],
+          },
+        },
+      ],
+      attributes: {
+        exclude: ["created_at", "updated_at"],
+      },
+    });
+
+    if (!bookings) {
+      return res.status(404).send({
+        success: false,
+        message: "No bookings found",
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Bookings retrieved successfully",
+      data: bookings,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: `Error: ${error.message}`,
+    });
+  }
+};
+
+const getBookingById = async (req, res, _next) => {
+  try {
+    const { id } = req.params;
+
+    const booking = await Bookings.findOne({
+      where: { id },
+      include: [
+        {
+          model: Travel_Packages,
+          as: "travel_packages",
+          attributes: {
+            exclude: ["created_at", "updated_at"],
+          },
+        },
+      ],
+      attributes: {
+        exclude: ["created_at", "updated_at"],
+      },
+    });
+
+    if (!booking) {
+      return res.status(404).send({
+        success: false,
+        message: "No booking found",
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Booking retrieved successfully",
+      data: booking,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: `Error: ${error.message}`,
     });
   }
 };
@@ -75,7 +195,7 @@ const completeBooking = async (req, res, _next) => {
       const { id } = req.params;
 
       const booking = await Bookings.findOne({ where: { id } });
-      
+
       if (!booking) {
         return res.status(404).send({
           success: false,
@@ -153,4 +273,10 @@ const completeBooking = async (req, res, _next) => {
   }
 };
 
-module.exports = { createBooking, completeBooking };
+module.exports = {
+  createBooking,
+  completeBooking,
+  getAllBookings,
+  getBookingHistories,
+  getBookingById,
+};
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { verifyToken } = require("../middleware/auth");
 const { updateProfile, deleteProfile, selfUpdatePassword, getAuthenticatedUser } = require("../controllers/user.controller");
+const { getAllBookings, getBookingHistories, getBookingById } = require("../controllers/booking.controller");
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.get("/", verifyToken, getAuthenticatedUser);
 router.patch("/", verifyToken, updateProfile);
 router.delete("/", verifyToken, deleteProfile);
 router.put("/password", verifyToken, selfUpdatePassword);
+router.get("/booking-history/:limit&:pages", verifyToken, getBookingHistories);
+router.get("/booking-history/:id", verifyToken, getBookingById);
 
 module.exports = router;
