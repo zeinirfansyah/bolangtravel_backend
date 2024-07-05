b03f6757dfa8b756558f5b18585aaa466692ae2f
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-05 23:17:50 +0700

    feat + refactor [booking]: added updateBooking and deleteBooking, moved getBookingHistories route, getBookingById route, from account.routes.js to booking.routes.js

--- src/routes/booking.routes.js
+++ src/routes/booking.routes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { verifyToken, authorizeRole } = require("../middleware/auth");
-const { createBooking,completeBooking, getAllBookings, getBookingById } = require("../controllers/booking.controller");
+const { createBooking,completeBooking, getAllBookings, getBookingById, updateBooking, getBookingHistories, deleteBooking } = require("../controllers/booking.controller");
 
 const router = express.Router();
 
-router.post("/", verifyToken, authorizeRole("admin"), createBooking);
+router.post("/", verifyToken, createBooking);
+router.get("/booking-history/:limit&:pages", verifyToken, getBookingHistories);
+router.put("/complete-booking/:id", verifyToken, completeBooking);
+router.get("/:id", verifyToken, getBookingById);
+
 router.get("/table/:limit&:pages", verifyToken, authorizeRole("admin"), getAllBookings);
-router.get("/:id", verifyToken, authorizeRole("admin"), getBookingById);
-router.patch("/:id", verifyToken, authorizeRole("admin"), completeBooking);
+router.patch("/:id", verifyToken, authorizeRole("admin"), updateBooking);
+router.delete("/:id", verifyToken, authorizeRole("admin"), deleteBooking);
 
 module.exports = router;
