865e2ba9b906b3210e43b0c8794615565818b926
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-25 07:06:39 +0700

    refactor [models]: added some column to database

--- src/database/migrations/20240620035340-create_destination.js
+++ src/database/migrations/20240620035340-create_destination.js
@@ -1,34 +1,53 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Destinations', {
+    await queryInterface.createTable("Destinations", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+      },
+      location: {
+        type: Sequelize.STRING,
+      },
+      ticket_price: {
+        type: Sequelize.INTEGER,
+      },
+      open_hour: {
+        type: Sequelize.STRING,
+      },
+      destination_contact: {
+        type: Sequelize.STRING,
+      },
+      is_available: {
+        type: Sequelize.ENUM("true", "false"),
+        defaultValue: "true",
+      },
+      expiry_date: {
+        type: Sequelize.DATE,
       },
       thumbnail: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updated_at: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Destinations');
-  }
+    await queryInterface.dropTable("Destinations");
+  },
 };
--- src/models/destinations.js
+++ src/models/destinations.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Destinations extends Model {
     /**
@@ -13,20 +11,32 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
 
       Destinations.belongsToMany(models.Travel_Packages, {
-        through: 'Travel_Packages_Destinations',
-        as: 'travel_packages',
-        foreignKey: 'destination_id',
+        through: "Travel_Packages_Destinations",
+        as: "travel_packages",
+        foreignKey: "destination_id",
       });
     }
   }
-  Destinations.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    thumbnail: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Destinations',
-    freezeTableName: true
-  });
+  Destinations.init(
+    {
+      title: DataTypes.STRING,
+      description: DataTypes.STRING,
+      location: DataTypes.STRING,
+      ticket_price: DataTypes.INTEGER,
+      open_hour: DataTypes.STRING,
+      destination_contact: DataTypes.STRING,
+      is_available: {
+        type: DataTypes.ENUM("true", "false"),
+        defaultValue: "true",
+      },
+      expiry_date: DataTypes.DATE,
+      thumbnail: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Destinations",
+      freezeTableName: true,
+    }
+  );
   return Destinations;
 };
