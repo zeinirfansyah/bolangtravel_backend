977208916766e8ebe0fb124990538b2059764800
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-07-25 07:07:31 +0700

    refactor [destination.controller & destination.routes]: added search and filter feature

--- src/routes/destination.routes.js
+++ src/routes/destination.routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { verifyToken, authorizeRole } = require("../middleware/auth");
-const {createDestinations, getAllDestinations, updateDestination, getDestinationById, deleteDestination } = require("../controllers/destination.controller");
+const {
+  createDestinations,
+  getAllDestinations,
+  updateDestination,
+  getDestinationById,
+  deleteDestination,
+} = require("../controllers/destination.controller");
 
 const router = express.Router();
 
-router.get("/", getAllDestinations);
+router.get("/table/:limit&:pages", getAllDestinations);
 router.get("/:id", getDestinationById);
 router.post("/", verifyToken, authorizeRole("admin"), createDestinations);
 router.patch("/:id", verifyToken, authorizeRole("admin"), updateDestination);
